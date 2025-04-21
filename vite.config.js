66f2a306debfbb2c2688bdea1401a36aeaec2673
keysijones <keysi.fernandes@clevertech.biz>
2025-04-21 18:41:57 -0300

    project update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/minimalistic-todo-list/',
+  base: '/todo-list-extension/',
 })
